3e219830ade01c99949d0b1c08859a53890e1511
youssefyou <youssef.ajdigue@gmail.com>
2025-06-10 16:48:15 +0200

    footer component

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    // liste de liens nous menant vers une page spécifique
+    <nav>
+      <ul>
+        <li>
+          <a href="/mentions-legales">Mentions légales</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+        <li>
+          <a href="/a-propos">À propos</a>
+        </li>
+        <li>
+          <a href="/politique-de-confidentialite">Politique de confidentialité</a>
+        </li>
+        <li>
+          <a href="/cgu">CGU</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
