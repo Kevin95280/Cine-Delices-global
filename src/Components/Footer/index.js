e70ca583998a887c21a77251f17caf3360a80b1b
youssefyou <youssef.ajdigue@gmail.com>
2025-06-10 19:14:44 +0200

    navbar_component

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -3,24 +3,28 @@ import React from "react";
 export default function Footer() {
   return (
     // liste de liens nous menant vers une page spécifique
-    <nav>
-      <ul>
-        <li>
-          <a href="/mentions-legales">Mentions légales</a>
-        </li>
-        <li>
-          <a href="/contact">Contact</a>
-        </li>
-        <li>
-          <a href="/a-propos">À propos</a>
-        </li>
-        <li>
-          <a href="/politique-de-confidentialite">Politique de confidentialité</a>
-        </li>
-        <li>
-          <a href="/cgu">CGU</a>
-        </li>
-      </ul>
-    </nav>
+    <footer>
+      <nav>
+        <ul>
+          <li>
+            <a href="/mentions-legales">Mentions légales</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+          <li>
+            <a href="/a-propos">À propos</a>
+          </li>
+          <li>
+            <a href="/politique-de-confidentialite">
+              Politique de confidentialité
+            </a>
+          </li>
+          <li>
+            <a href="/cgu">CGU</a>
+          </li>
+        </ul>
+      </nav>
+    </footer>
   );
 }
--- src/Components/Header/NavBar/index.js
+++ src/Components/Header/NavBar/index.js
@@ -1,5 +1,22 @@
 import React from "react";
 
 export default function NavBar() {
-    
+  return (
+    // composant navbar pour la navigation "Recettes" et "Films & Séries"
+    <nav>
+      <ul>
+        <li>
+          <a href="#">
+            <img src="#" alt="logo cine-delices" />
+          </a>
+        </li>
+        <li>
+          <a href="/recettes">Recettes</a>
+        </li>
+        <li>
+          <a href="/films-series">Films & Séries</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
