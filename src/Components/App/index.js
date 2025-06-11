20994968f639d29eb75a2c5e0f37d6e28e791819
youssefyou <youssef.ajdigue@gmail.com>
2025-06-11 15:02:21 +0200

    comments added and modification <a> tags in NavLink

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,7 +1,17 @@
 import React from "react";
+import NavBar from "../Header/NavBar";
+import Header from "../Header";
+import Footer from "../Footer";
 
 export default function App() {
-    return (
+  return (
+    <div>
+      <Header />
+      <NavBar />
+      <main>
         <h1>Test</h1>
-    )
+      </main>
+      <Footer />
+    </div>
+  );
 }
