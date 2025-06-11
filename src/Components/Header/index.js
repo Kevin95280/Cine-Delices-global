6e92dc20cc129ee4f0378d1f6c7746010ef3bdc6
youssefyou <youssef.ajdigue@gmail.com>
2025-06-11 15:30:14 +0200

    comments added

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,12 +1,14 @@
 import React from "react";
+// Import du composant NavBar
 import NavBar from "./NavBar";
 
 export default function Header() {
   return (
     <header>
-      {/* Appel de notre composant NavBar */}
+      {/* Appel de notre composant NavBar pour la navigation principale*/}
       <NavBar />
       <div>
+        {/* Chemin src à compléter avec l'image du logo */}
         <img src="#" alt="logo cine-delices" />
         <h1>Le goût du cinéma, dans votre assiette.</h1>
       </div>
