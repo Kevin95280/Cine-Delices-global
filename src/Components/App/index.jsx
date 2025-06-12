21dbbac68af6453f3d050f9b76e59eb73cb5673a
Kevin95280 <coelhokev@ymail.com>
2025-06-12 14:28:48 +0200

    Router configuration

--- src/Components/App/index.jsx
+++ src/Components/App/index.jsx
@@ -1,22 +1,5 @@
-// Import de nos différents composants
-import Header from "../Header";
-import NavBar from "../Header/NavBar";
-import Cards from "../Cards";
-import Card from "../Cards/Card";
-import Footer from "../Footer";
+import AppRouter from "../Router";
 
 export default function App() {
-    return (
-        <>
-            <Header>
-                <NavBar />
-            </Header>
-            <Cards title={'Top recettes'}>
-                <Card title={'Recette 1'} authorName={'toto'} />
-                <Card title={'Recette 2'} authorName={'toto'} />
-                <Card title={'Recette 3'} authorName={'toto'} />
-            </Cards>
-            <Footer />
-        </>
-    )
-} 
+  return <AppRouter />;
+}
---
+++ src/Components/Router.jsx
@@ -0,0 +1,61 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+/*import Recipes from "../pages/Recipes";
+import RecipeDetail from "../pages/RecipeDetail";
+import MoviesSeries from "../pages/MoviesSeries";
+import MovieDetail from "../pages/MovieDetail";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import ForgotPassword from "../pages/ForgotPassword";
+import ResetPassword from "../pages/ResetPassword";
+import MyAccount from "../pages/MyAccount";
+import MyRecipes from "../pages/MyRecipes";
+import AccountForm from "../pages/AccountForm";
+import SearchForm from "../pages/SearchForm";
+import FilterForm from "../pages/FilterForm";
+import Contact from "../pages/Contact";
+import AboutUs from "../pages/AboutUs";
+import LegalNotice from "../pages/LegalNotice";
+import PrivacyPolicy from "../pages/PrivacyPolicy";
+import TermsAndConditions from "../pages/TermsAndConditions";
+import NotFound from "../pages/NotFound"; // Page 404*/
+
+export default function AppRouter() {
+  return (
+    <Router>
+      <Routes>
+        {/* Pages principales */}
+        <Route path="/" element={<Home />} />
+        {/*<Route path="/recipes" element={<Recipes />} />
+        <Route path="/recipes/:recipe-id" element={<RecipeDetail />} />
+        <Route path="/movies-and-series" element={<MoviesSeries />} />
+        <Route path="/movies-and-series/:movie-id" element={<MovieDetail />} />*/}
+        
+        {/* Authentification */}
+        {/*<Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password/:token" element={<ResetPassword />} />*/}
+
+        {/* Gestion utilisateur */}
+        {/*<Route path="/my-account" element={<MyAccount />} />
+        <Route path="/my-recipes" element={<MyRecipes />} />*/}
+
+        {/* Formulaires */}
+        {/*<Route path="/account-form" element={<AccountForm />} />
+        <Route path="/search-form" element={<SearchForm />} />
+        <Route path="/filter-form" element={<FilterForm />} />*/}
+
+        {/* Pages statiques */}
+        {/*<Route path="/contact" element={<Contact />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/legal-notice" element={<LegalNotice />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/terms-and-conditions" element={<TermsAndConditions />} />*/}
+
+        {/* Page 404 */}
+        {/*<Route path="*" element={<NotFound />} />*/}
+      </Routes>
+    </Router>
+  );
+}
