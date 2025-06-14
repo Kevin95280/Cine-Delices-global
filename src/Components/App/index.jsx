58597e5242a44478ea73f45aae4e63b77943b3ee
KimYenNGUYEN <nguyen0kim@hotmail.com>
2025-06-14 16:51:13 +0200

    finalization for static version bis

--- src/Components/App/index.jsx
+++ src/Components/App/index.jsx
@@ -2,6 +2,5 @@ import LoginForm from "../AccountForm/LoginForm";
 import AppRouter from "../Router";
 
 export default function App() {
-  // return <AppRouter />;
-return <LoginForm />;
+  return <AppRouter />;
 }
