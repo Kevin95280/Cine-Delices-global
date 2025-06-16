bbdd377123f47f2e8bd6623832c8eedadffb0600
KimYenNGUYEN <nguyen0kim@hotmail.com>
2025-06-16 10:45:23 +0200

    App back to AppRouter after changed for AccountForm testing

--- src/Components/App/index.jsx
+++ src/Components/App/index.jsx
@@ -3,6 +3,5 @@ import SignupForm from "../AccountForm/SignupForm";
 import AppRouter from "../Router";
 
 export default function App() {
-  // return <AppRouter />;
-  return <SignupForm />
+  return <AppRouter />;
 }
