4373424b3398ca3612d94ea4e06217a8bad11edc
KimYenNGUYEN <nguyen0kim@hotmail.com>
2025-06-16 10:37:26 +0200

    LoginForm : try throw catch fixed

--- src/Components/App/index.jsx
+++ src/Components/App/index.jsx
@@ -1,6 +1,8 @@
 import LoginForm from "../AccountForm/LoginForm";
+import SignupForm from "../AccountForm/SignupForm";
 import AppRouter from "../Router";
 
 export default function App() {
-  return <AppRouter />;
+  // return <AppRouter />;
+  return <SignupForm />
 }
