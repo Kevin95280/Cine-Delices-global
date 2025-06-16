95d0c3367ff59cf6393bd656b9f30669454b1ed2
Anthony-Hannoteaux <tony.hannoteaux@gmail.com>
2025-06-16 11:57:43 +0200

    Fix conflicts with Router.jsx

--- src/Components/App/index.jsx
+++ src/Components/App/index.jsx
@@ -1,28 +1,5 @@
-// Test du rendu du composant formulaire
-import SearchForm from "../SearchForm";
-import Cards from "../Cards";
-import Card from "../Cards/Card";
-// On importe notre hook personnalisé
-import useSearch from "../../Hook/useSearch";
+import AppRouter from "../Router";
 
 export default function App() {
-
-  // On récupère les états et fonctions depuis le hook personnalisé
-  const { searchTerm, movies, handleChange, handleSubmit } = useSearch();
-
-  return (
-    <>
-      <SearchForm searchTerm={searchTerm} handleSubmit={handleSubmit} handleChange={handleChange} />
-      <Cards title="Test API, résultat de la recherche (Films)">
-        {movies.map((movie) => (
-          <Card
-            // Clé utile à React pour pouvoir afficher nos différents éléments
-            key={movie.id}
-            title={movie.title}
-            image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-          />
-        ))}
-      </Cards>
-    </>
-  )
+  return <AppRouter />;
 }
