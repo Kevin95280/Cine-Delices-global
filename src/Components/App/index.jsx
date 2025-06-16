b9c92b472d2d6986ea683bbb26f075936b3ab88f
Kim NGUYEN <nguyen0kim@hotmail.com>
2025-06-16 18:56:18 +0200

    Revert "Account form (#28)"
    This reverts commit 8bd0fc601b913979c75242edd2a9e0cdbd91a625.

--- src/Components/App/index.jsx
+++ src/Components/App/index.jsx
@@ -1,5 +1,3 @@
-import LoginForm from "../AccountForm/LoginForm";
-import SignupForm from "../AccountForm/SignupForm";
 import AppRouter from "../Router";
 
 export default function App() {
--- src/pages/Account/Signup.jsx
+++
@@ -1,5 +0,0 @@
-import SignupForm from "../../Components/AccountForm/SignupForm";
-
-export default function Signup () {
-    <SignupForm />
-}
