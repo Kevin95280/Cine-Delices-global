9039f2886ee963aa730dd483af98c456c36467e6
youssefyou <youssef.ajdigue@gmail.com>
2025-06-17 15:34:13 +0200

    add-recipe

--- src/pages/Food/AddRecipe.jsx
+++ src/pages/Food/AddRecipe.jsx
@@ -1,10 +1,42 @@
 import { Helmet } from "react-helmet";
+import { useState } from "react";
 import Header from "../../Components/Header";
 import NavBar from "../../Components/Header/NavBar";
 import SearchForm from "../../Components/Header/SearchForm";
 import Footer from "../../Components/Footer";
 
 export default function AddRecipe() {
+  // déclarations des étatas pour chaque champ du formulaire (valeur et fonction)
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [type, setType] = useState("");
+  const [level, setLevel] = useState("");
+  const [budget, setBudget] = useState("");
+  const [servings, setServings] = useState("");
+  const [preparationTime, setPreparationTime] = useState("");
+  const [cookingTime, setCookingTime] = useState("");
+  const [ingredient, setIngredient] = useState("");
+  const [step, setStep] = useState("");
+  const [story, setStory] = useState("");
+  const [picture, setPicture] = useState("");
+
+  // soumission de notre formulaire
+  const handleSubmit = (e) => {
+    // empêche le rechargement de la page
+    e.preventDefault();
+    // test pour s'assurer qu'on récupère bien nos variables d'état au submit
+    alert(`Submitted ${title} ${description}`);
+  };
+
+  // test boutton
+  const AddIngredient = () => {
+    alert("Ingrédient ajouté");
+  };
+
+  const AddStep = () => {
+    alert("Etape ajoutée");
+  };
+
   return (
     <>
       {/* Helmet gère le contenu de la balise head, ici le titre ainsi que nos meta données afin d'améliorer le référencement (SEO)*/}
@@ -26,7 +58,13 @@ export default function AddRecipe() {
       <main className="main">
         <section className="form-recipe">
           <h1 className="form__title">Formulaire de publication recette :</h1>
-          <form className="form__container">
+          <form
+            className="form__container"
+            // utilisation de la méthode POST pour demander une réponse à notre serveur
+            method="POST"
+            // fonction executée pour soummettre le formulaire lorsque l'utilisateur clique sur le bouton envoyer ou appuie sur la touche Entrée
+            onSubmit={handleSubmit}
+          >
             <label htmlFor="title" className="form__label">
               Titre de la recette
               <input
@@ -35,6 +73,8 @@ export default function AddRecipe() {
                 id="title"
                 name="title"
                 aria-required="true"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
               />
             </label>
             <label htmlFor="description" className="form__label">
@@ -43,6 +83,10 @@ export default function AddRecipe() {
                 className="form__textarea"
                 id="description"
                 name="description"
+                // valeur de notre input
+                value={description}
+                // mise à jour de l'état à chaque changement
+                onChange={(e) => setDescription(e.target.value)}
               />
             </label>
             {/* Informations générales */}
@@ -54,6 +98,8 @@ export default function AddRecipe() {
                 id="type"
                 name="type"
                 aria-required="true"
+                value={type}
+                onChange={(e) => setType(e.target.value)}
               />
             </label>
             <label htmlFor="level" className="form__label">
@@ -63,6 +109,8 @@ export default function AddRecipe() {
                 type="text"
                 id="level"
                 name="level"
+                value={level}
+                onChange={(e) => setLevel(e.target.value)}
               />
             </label>
             <label htmlFor="budget" className="form__label">
@@ -72,6 +120,8 @@ export default function AddRecipe() {
                 type="text"
                 id="budget"
                 name="budget"
+                value={budget}
+                onChange={(e) => setBudget(e.target.value)}
               />
             </label>
             <label htmlFor="servings" className="form__label">
@@ -82,6 +132,8 @@ export default function AddRecipe() {
                 id="servings"
                 name="servings"
                 aria-required="true"
+                value={servings}
+                onChange={(e) => setServings(e.target.value)}
               />
             </label>
             <label htmlFor="preparation-time" className="form__label">
@@ -92,6 +144,8 @@ export default function AddRecipe() {
                 id="preparation-time"
                 name="preparation-time"
                 aria-required="true"
+                value={preparationTime}
+                onChange={(e) => setPreparationTime(e.target.value)}
               />
             </label>
             <label htmlFor="cooking-time" className="form__label">
@@ -102,60 +156,56 @@ export default function AddRecipe() {
                 id="cooking-time"
                 name="cooking-time"
                 aria-required="true"
+                value={cookingTime}
+                onChange={(e) => setCookingTime(e.target.value)}
               />
             </label>
             {/* Ingrédients */}
             <label htmlFor="ingredient-1" className="form__label">
-              Ingrédient 1
+              Ingrédient
               <input
                 className="form__input"
                 type="text"
                 id="ingredient-1"
                 name="ingredient-1"
                 aria-required="true"
+                value={ingredient}
+                onChange={(e) => setIngredient(e.target.value)}
               />
             </label>
-            <label htmlFor="ingredient-2" className="form__label">
-              Ingrédient 2
-              <input
-                className="form__input"
-                type="text"
-                id="ingredient-2"
-                name="ingredient-2"
-                aria-required="true"
-              />
-            </label>
-            <button type="button" className="form__button">
+            <button
+              type="button"
+              className="form__button"
+              onClick={AddIngredient}
+            >
               Ajouter un ingrédient
             </button>
-            {/* Etapes de préparation*/}
+            {/* Étapes de préparation*/}
             <label htmlFor="step-1" className="form__label">
-              Étape 1
+              Étape
               <input
                 className="form__input"
                 type="text"
                 id="step-1"
                 name="step-1"
                 aria-required="true"
+                value={step}
+                onChange={(e) => setStep(e.target.value)}
               />
             </label>
-            <label htmlFor="step-2" className="form__label">
-              Étape 2
-              <input
-                className="form__input"
-                type="text"
-                id="step-2"
-                name="step-2"
-                aria-required="true"
-              />
-            </label>
-            <button type="button" className="form__button">
+            <button type="button" className="form__button" onClick={AddStep}>
               Ajouter une étape
             </button>
             {/* Anecdote */}
             <label htmlFor="story" className="form__label">
               Anecdote
-              <textarea className="form__textarea" id="story" name="story" />
+              <textarea
+                className="form__textarea"
+                id="story"
+                name="story"
+                value={story}
+                onChange={(e) => setStory(e.target.value)}
+              />
             </label>
             <div className="form__picture">
               <label htmlFor="picture" className="form__label">
@@ -166,10 +216,12 @@ export default function AddRecipe() {
                 type="file"
                 id="picture"
                 name="picture"
+                value={picture}
+                onChange={(e) => setPicture(e.target.value)}
               />
               {/* Soumission du formulaire */}
               <button type="submit" className="form__button">
-                Soumettre
+                Envoyer
               </button>
             </div>
           </form>
