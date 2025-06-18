13d61b77c3b542042799c98ff80d3ecdc19cca34
Anthony-Hannoteaux <tony.hannoteaux@gmail.com>
2025-06-18 09:50:14 +0200

    Retrieve checkbox values OK

--- src/Components/FilterForm/QuickFinder/index.jsx
+++ src/Components/FilterForm/QuickFinder/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+
 export default function QuickFinder() {
     const data = [
         {
@@ -16,7 +17,6 @@ export default function QuickFinder() {
             }
         }
     ]
-
     /**
      * Variable d'état qui vérifie l'état de la checkbox
      * False en état initial
@@ -24,25 +24,40 @@ export default function QuickFinder() {
     const [isChecked, setIsChecked] = useState(false)
     // Variable d'état qui gère la récupération de la valeur de la checkbox
     const [isValue, setIsValue] = useState('valeur')
+    
+    /**
+     * Handler - Action à la soumission
+     * @param {string} value - Valeur de la checkbox lors d'un clique
+     */
+    const handleSubmit = (value) => {
+        // Affichage de la valeur de la checkbox coché en alert
+        alert(`Valeur de la checkbox: ${value}`)
+    }
     /**
      * Handler - Permet de modifier la valeur de notre état
      * Evènement associé à l'input
      */
     const handleChange = (e) => {
+        // Modification de la variable d'état de la checkbox au clique
         setIsChecked(!isChecked);
+        // Condition, si la checkbox est coché alors
+        if (e.target.checked) {
+            // On stock la valeur de l'attribut name dans une constante
+            const value = e.target.name;
+            // On modifie la variable d'état avec la valeur récupéré
+            setIsValue(value)
+            // On passe en argument cette même valeur lors de la soumission
+            handleSubmit(value)
+        }
     }
-
     return (
-    // Fonction pas encore initialisé pour la soumission
-    <form method="POST" onSubmit={(e) => null}>
+    <form method="POST" onSubmit={handleSubmit}>
         <div>
             <input
             type="checkbox"
             id="test"
             name="test"
-            // Ce qui nous permet de contrôler notre input et donc de modifier grâce au booléen si checké ou non
             checked={isChecked}
-            // Evènement associé
             onChange={handleChange}
             />
             <label htmlFor="test">Test</label>
