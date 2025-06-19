92c820c83316fe3478a36fba66b405fd64b994e6
Anthony-Hannoteaux <tony.hannoteaux@gmail.com>
2025-06-19 23:32:38 +0200

    Initial styling for Cards and Card components

--- src/Components/App/index.jsx
+++ src/Components/App/index.jsx
@@ -1,7 +1,25 @@
-import LoginForm from "../AccountForm/LoginForm";
-import SignupForm from "../AccountForm/SignupForm";
+import Cards from "../Cards";
+import Card from "../Cards/Card";
 import AppRouter from "../Router";
 
 export default function App() {
-  return <AppRouter />;
+    return (
+        <Cards title={"Top recettes"}>
+            <Card 
+            title={"Pancake aux fruits"}
+            authorName={"Nom_Auteur"}
+            image={"/assets/image-test.jpg"}
+            />
+            <Card 
+            title={"Pancake aux fruits"}
+            authorName={"Nom_Auteur"}
+            image={"/assets/image-test.jpg"}
+            />
+            <Card 
+            title={"Pancake aux fruits"}
+            authorName={"Nom_Auteur"}
+            image={"/assets/image-test.jpg"}
+            />
+        </Cards>
+    )
 }
--- src/Components/Cards/Card/index.jsx
+++ src/Components/Cards/Card/index.jsx
@@ -1,9 +1,11 @@
+import "./style.scss";
+
 export default function Card({ title, authorName, image }) {
     return (
         <div className="card">
             <div className="card__header">
                 {/* Image statique pour les tests d'affichage */}
-                <img src="assets/image-test.jpg" alt="Image test" className="card__image"/>
+                <img src={image} alt="Image test" className="card__image"/>
             </div>
             <div className="card__body">
                 <h4 className="card__title">{ title }</h4>
