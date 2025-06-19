41ad28218fa059935e230676b9ac32cd260c4e17
youssefyou <youssef.ajdigue@gmail.com>
2025-06-19 10:53:34 +0200

    label and sr-only added

--- src/Components/Header/SearchForm/index.jsx
+++ src/Components/Header/SearchForm/index.jsx
@@ -4,6 +4,10 @@ export default function SearchForm({ searchTerm, handleSubmit, handleChange }) {
     // Prop d'événement qui fera appel à notre fonction handleSubmit à la soumission
     <form onSubmit={handleSubmit} className="search__form">
       {/* Prop d'événement qui gère l'état de la valeur du champ de recherche */}
+      <label
+        htmlFor="search-form-label"
+        className="search__form__label sr-only"
+      ></label>
       <input
         onChange={handleChange}
         type="text"
