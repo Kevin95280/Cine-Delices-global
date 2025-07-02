e523f514d8899b1b5cb14bcf1bff19b96a27acb1
Anthony-Hannoteaux <tony.hannoteaux@gmail.com>
2025-07-02 23:13:40 +0200

    Improve accessibility on login and login component

--- src/Components/AccountForm/LoginForm/index.jsx
+++ src/Components/AccountForm/LoginForm/index.jsx
@@ -87,38 +87,47 @@ export default function LoginForm() {
       method="POST"
       onSubmit={handleSubmit}
     >
-      <label htmlFor="email" className="account__form__label">
-        Email
-        <input
-          className="account__form__input"
-          type="email"
-          id="email"
-          name="email"
-          // prise en compte de la valeur du champ une fois rempli, ici spécifique à l'email
-          // puis par la suite adapté en fonction de la valeur des autres champs
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-          placeholder="nom@domaine.extension"
-          required
-        />
-      </label>
-      <label htmlFor="password" className="account__form__label">
-        Mot de passe
-        <input
-          className="account__form__input"
-          type="password"
-          id="password"
-          name="password"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-          minLength={12}
-          placeholder="Votre mot de passe"
-          required
-        />
-      </label>
-      <button type="submit" className="account__form__button">
-        Connexion
-      </button>
+      {/* Ajout d'un fieldset, pour mieux structurer sémantiquement le formulaire */}
+      <fieldset>
+        {/* Ajout d'une légende, qui sera "caché" visuellement, mais accessible via une lisseuse d'écran*/}
+        <legend className="sr-only">Formulaire de connexion :</legend>
+        <p>Tout les champs sont obligatoire</p>
+        <label htmlFor="email" className="account__form__label">
+          <span>Email :</span>
+          {/* Indication sur le format attendu accessible */}
+          <span className="sr-only">Exemple : nom@domaine.extension</span>
+          <input
+            className="account__form__input"
+            type="email"
+            id="email"
+            name="email"
+            // prise en compte de la valeur du champ une fois rempli, ici spécifique à l'email
+            // puis par la suite adapté en fonction de la valeur des autres champs
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            placeholder="nom@domaine.extension"
+            required
+          />
+        </label>
+        <label htmlFor="password" className="account__form__label">
+          <span>Mot de passe :</span>
+          <input
+            aria-label="Veillez entrer votre mot de passe"
+            className="account__form__input"
+            type="password"
+            id="password"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            minLength={12}
+            placeholder="Votre mot de passe"
+            required
+          />
+        </label>
+        <button type="submit" className="account__form__button">
+          Connexion
+        </button>
+      </fieldset>
     </form>
   );
 }
