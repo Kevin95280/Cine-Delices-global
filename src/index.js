3c0cd1018d8143fadd8158fd19b62daa0f03b0ab
Anthony-Hannoteaux <tony.hannoteaux@gmail.com>
2025-07-08 23:58:33 +0200

    AuthContext created, test and verify with LoginForm component

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ import { createRoot } from "react-dom/client";
 // Import du composant App, qui s'occupera de renvoyer notre rendu
 import App from "./Components/App";
 
+// Import de notre contexte
+import { AuthProvider } from "./Authentification";
+
 // Nous ciblons notre élément racine
 const rootContainer = createRoot(document.getElementById('root'));
 
 // Nous renvoyons le rendu dans notre élément racine
-rootContainer.render(<App />);
+rootContainer.render(
+    // Tous les composants enfants hériterons des valeurs du contexte
+    <AuthProvider>
+        <App />
+    </AuthProvider>
+);
