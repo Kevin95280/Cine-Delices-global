b74e2a2204bc99f1c7fa58885f308647261b859e
Kevin95280 <coelhokev@ymail.com>
2025-07-10 18:30:55 +0200

    MyRecipes page and Array component dynamisation

--- src/Components/Array/index.jsx
+++ src/Components/Array/index.jsx
@@ -1,36 +1,40 @@
 // Utilisation de props
 // Dans un premier temps pour gérer le titre de niveau 2 lors de l'affichage du rendu
 export default function Array({ title, data }) {
-    return (
-        <>
-            <div className="array-container">
-                <h1>{ title }</h1>
-                <div className="table-wrapper">
-                    <table>
-                        {/* Possibilité de ne pas le faire apparaître avec une classe sr-only pour garder le côté accessible */}
-                        <caption className="sr-only">
-                            Toutes les publications de mes différentes recettes
-                        </caption>
-                        <thead>
-                            {/* Gestion dynamique des titres des colonnes en fonction de la page où le composant sera utilisé */}
-                            <tr>
-                                {/* Pour le test du premier rendu, utilisation statique de contenu */}
-                                {data.map((item, index) => (
-                                    <th key={`label-${index}`}>{item.label}</th>
-                                ))}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {/* Ici, gestion dynamique de l'affichage en fonction des données récupéré */}
-                            <tr>
-                                {data.map((item, index) => (
-                                    <td key={`value-${index}`}>{item.value}</td>
-                                ))}
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </>
-    )
+  const isMultiRow = data.length > 0 && typeof data[0] === "object" && !data[0].label;
+
+  return (
+    <div className="array-container">
+      <h2>{title}</h2>
+      <div className="table-wrapper">
+        <table>
+          <caption className="sr-only">{title}</caption>
+          <thead>
+            <tr>
+              {isMultiRow
+                ? Object.keys(data[0]).map((key, index) => <th key={`th-${index}`}>{key}</th>)
+                : data.map((item, index) => <th key={`th-${index}`}>{item.label}</th>)}
+            </tr>
+          </thead>
+          <tbody>
+            {isMultiRow ? (
+              data.map((row, i) => (
+                <tr key={`row-${i}`}>
+                  {Object.values(row).map((value, j) => (
+                    <td key={`td-${i}-${j}`}>{value}</td>
+                  ))}
+                </tr>
+              ))
+            ) : (
+              <tr>
+                {data.map((item, index) => (
+                  <td key={`td-${index}`}>{item.value}</td>
+                ))}
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
