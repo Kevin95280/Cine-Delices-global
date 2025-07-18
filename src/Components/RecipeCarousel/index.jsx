f68f3467709ead470b7d830915f67600328d7b9f
Kevin95280 <coelhokev@ymail.com>
2025-07-18 15:44:10 +0200

    Carousel improvment

--- src/Components/RecipeCarousel/index.jsx
+++ src/Components/RecipeCarousel/index.jsx
@@ -1,24 +1,46 @@
+import { useEffect } from "react";
 import Slider from "react-slick";
 import Card from "../Cards/Card";
 
-const RecipeCarousel = ({ title, recipes }) => {
+export default function RecipeCarousel({ title, recipes }) {
   const settings = {
     dots: false,
     infinite: true,
     speed: 400,
-    slidesToShow: 3,
-    slidesToScroll: 1,
+    slidesToShow: 9,
+    slidesToScroll: 3,
     responsive: [
       {
         breakpoint: 768,
-        settings: { slidesToShow: 2 },
+        settings: { slidesToShow: 2, slidesToScroll: 2 },
       },
       {
         breakpoint: 480,
-        settings: { slidesToShow: 1 },
+        settings: { slidesToShow: 1, slidesToScroll: 1},
       },
     ],
   };
+
+  // Précharge les images du carousel
+  useEffect(() => {
+    const preloadImage = (url) => {
+      const img = new Image();
+      img.src = url;
+      img.decode().catch(() => {}); // évite les blocages JS
+    };
+
+    if (recipes && recipes.length > 0) {
+      recipes.forEach((recipe) => {
+        if (recipe.picture_url) preloadImage(recipe.picture_url);
+      });
+    }
+  }, [recipes]);
+
+// Fonction utilitaire pour tronquer le texte si nécessaire
+// Limite le titre à XX caractères et ajoute "…" si nécessaire
+  const truncate = (text, maxLength) => {
+  return text.length > maxLength ? text.slice(0, maxLength) + "…" : text;
+};
 
   return (
     <section className="carousel-section">
@@ -28,7 +50,7 @@ const RecipeCarousel = ({ title, recipes }) => {
           <Card
             key={recipe.id}
             id={recipe.id}
-            title={recipe.title}
+            title={truncate(recipe.title, 18)}
             authorName={recipe.author_username}
             image={recipe.picture_url}
           />
@@ -37,5 +59,3 @@ const RecipeCarousel = ({ title, recipes }) => {
     </section>
   );
 };
-
-export default RecipeCarousel;
