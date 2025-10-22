e963cdd166c182e15e0de2f785884176148e8ee2
Kevin95280 <coelhokev@ymail.com>
2025-10-22 18:28:31 +0200

    database.js modif

--- backend/src/app/database.js
+++ backend/src/app/database.js
@@ -1,13 +1,16 @@
 import pg from "pg";
-// import * as dotenv from "dotenv";
 import dotenv from "dotenv";
-                      
-dotenv.config();      
-                      
-                      
-// Fichier .env n'est pas lu.. à corriger avant de push !
-const client = new pg.Client();
-                      
-client.connect();     
-                      
+
+dotenv.config();
+
+const client = new pg.Client({
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+});
+
+client.connect();
+
 export default client;
